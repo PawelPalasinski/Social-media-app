8a68c67e7d642517ef1afad84cc6aaf52b47a825
PawelPalasinski <pawel.palasinski@gmail.com>
2022-07-02 20:09:22 +0200

    clean code

--- main.js
+++ main.js
@@ -4,50 +4,46 @@ const msg = document.querySelector("#msg");
 const posts = document.querySelector("#posts");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("it works");
-    formValidation();
-})
+  e.preventDefault();
+  console.log("it works");
+  formValidation();
+});
 
 let formValidation = () => {
-    if (input.value === "") {
-        console.log("failure");
-        msg.innerHTML = "Post cannot be clank";
-     }
-    else {
-        console.log("success");
-        msg.innerHTML = "";
-        acceptData();
-
-     }
+  if (input.value === "") {
+    console.log("failure");
+    msg.innerHTML = "Post cannot be clank";
+  } else {
+    console.log("success");
+    msg.innerHTML = "";
+    acceptData();
+  }
 };
 
 let data = {};
 
 let acceptData = () => {
-    data["text"] = input.value;
-    console.log(data);
-    createPost();
+  data["text"] = input.value;
+  console.log(data);
+  createPost();
 };
 
 let createPost = () => {
-    posts.innerHTML += 
-        `<div>
+  posts.innerHTML += `<div>
         <p>${data.text}</p>
         <span class="options">
         <i onClick="editPost(this)" class="fas fa-edit"></i>
         <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
         </span>
         </div>`;
-    input.value = "";
-
-}
+  input.value = "";
+};
 
 let deletePost = (e) => {
-    e.parentElement.parentElement.remove();
-}
+  e.parentElement.parentElement.remove();
+};
 
 let editPost = (e) => {
-    input.value = e.parentElement.previousElementSibling.innerHTML;
-    e.parentElement.parentElement.remove();
-}
+  input.value = e.parentElement.previousElementSibling.innerHTML;
+  e.parentElement.parentElement.remove();
+};
