c0b7af98fe8ece5ecb95d7aa702b043e1a6eb1ab
PawelPalasinski <pawel.palasinski@gmail.com>
2022-07-02 19:53:25 +0200

    C - create

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#form");
 const input = document.querySelector("#input");
 const msg = document.querySelector("#msg");
+const posts = document.querySelector("#posts");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -17,6 +18,7 @@ let formValidation = () => {
         console.log("success");
         msg.innerHTML = "";
         acceptData();
+
      }
 };
 
@@ -25,4 +27,22 @@ let data = {};
 let acceptData = () => {
     data["text"] = input.value;
     console.log(data);
+    createPost();
 };
+
+let createPost = () => {
+    posts.innerHTML += 
+        `<div>
+        <p>${data.text}</p>
+        <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+        </span>
+        </div>`;
+    input.value = "";
+
+}
+
+let deletePost = (e) => {
+    e.remove();
+}
